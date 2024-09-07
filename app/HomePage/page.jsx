f9a4c4e0845069e433ce494d5faa4b8cca08191c
santhosh2852 <santhoshmetheofficial@gmail.com>
2024-09-07 15:47:43 +0530

    commit check

--- app/HomePage/page.jsx
+++ app/HomePage/page.jsx
@@ -6,8 +6,8 @@ const HomePage = () => {
   return (
     <div className="select-none">
       <NavBar/>
-      <div className=''>
-        <h1 className=''>Jaya Kumar</h1>
+      <div className='bg-violet-500'>
+        <h1 className='text-lg font-extralight'>Jaya Kumar</h1>
         <h2 className=''>Graphic Designer</h2>
         <p className=''>I am a professional graphic designer with 7 months of experience in the industry. Specializing in specific areas like Social Media Post, Day Wishes post And Product post I have a proven track record of success in delivering highquality designs that meet clients' needs. With expertise in list specific skills like Adobe Photoshop, Illustrator, Canva, etc. I am dedicated to creating visually appealing and effective solutions for businesses. Passionate about design</p>
       </div>
