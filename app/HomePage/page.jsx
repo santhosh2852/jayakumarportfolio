ab369a9e33d5c0b4472bca3b5d70352f0f650f5a
santhosh2852 <santhoshmetheofficial@gmail.com>
2024-09-08 11:19:22 +0530

    check

--- app/HomePage/page.jsx
+++ app/HomePage/page.jsx
@@ -4,12 +4,12 @@ import React from 'react'
 import NavBar from '../NavBar/page';
 const HomePage = () => {
   return (
-    <div className="select-none">
+    <div className="p-10 m-0 select-none bg-violet-500">
       <NavBar/>
-      <div className='bg-violet-500'>
-        <h1 className='text-lg font-extralight'>Jaya Kumar</h1>
-        <h2 className=''>Graphic Designer</h2>
-        <p className=''>I am a professional graphic designer with 7 months of experience in the industry. Specializing in specific areas like Social Media Post, Day Wishes post And Product post I have a proven track record of success in delivering highquality designs that meet clients' needs. With expertise in list specific skills like Adobe Photoshop, Illustrator, Canva, etc. I am dedicated to creating visually appealing and effective solutions for businesses. Passionate about design</p>
+      <div className='pt-10'>
+        <h1 className='font-bold text-black text-7xl'>Jaya Kumar</h1>
+        <h2 className='font-semibold'>Graphic Designer</h2>
+        <p className='font-medium'>I am a professional graphic designer with 7 months of experience in the industry. Specializing in specific areas like Social Media Post, Day Wishes post And Product post I have a proven track record of success in delivering highquality designs that meet clients' needs. With expertise in list specific skills like Adobe Photoshop, Illustrator, Canva, etc. I am dedicated to creating visually appealing and effective solutions for businesses. Passionate about design</p>
       </div>
     </div>
   )
